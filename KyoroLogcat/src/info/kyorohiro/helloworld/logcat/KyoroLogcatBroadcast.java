7d27898dc4eba5a460a0858ddeb26d36532baf7b
kyorohiro <kyorohiro@gmail.com>
2012-02-04 21:33:51 +0900

    KyoroLogcat (alpha) version 114

---
+++ KyoroLogcat/src/info/kyorohiro/helloworld/logcat/KyoroLogcatBroadcast.java
@@ -0,0 +1,13 @@
+package info.kyorohiro.helloworld.logcat;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class KyoroLogcatBroadcast extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+	}
+
+}
