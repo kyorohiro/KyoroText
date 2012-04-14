7bcaaa058641695ef05d8b4c0774ba26a9ca294f
kyorohiro <kyorohiro@gmail.com>
2012-04-14 13:02:06 +0900

    KyoroStress(0.1080:18)

--- KyoroStress/src/info/kyorohiro/helloworld/util/KyoroMemoryInfo.java
+++ KyoroStress/src/info/kyorohiro/helloworld/util/KyoroMemoryInfo.java
@@ -1,5 +1,8 @@
 package info.kyorohiro.helloworld.util;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import android.app.ActivityManager;
@@ -8,6 +11,7 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Environment;
 import android.util.Log;
+import android.view.MotionEvent;
 
 public class KyoroMemoryInfo {
 	public List<RunningAppProcessInfo>  getRunningAppList(Context context) {
@@ -42,30 +46,27 @@ public class KyoroMemoryInfo {
 	    		//",nsd=" + info.nativeSharedDirty +
 	    		//",opd=" + info.otherPrivateDirty +
 	    		//",op=" + info.otherPss +
-	    		",TPD=" + info.getTotalPrivateDirty()+
-	    		"kb,TPss=" + info.getTotalPss() + 
-	    		"kb,TSD=" + info.getTotalSharedDirty()+"kb";
+	    		",TPD=" + getTotalPrivateDirty(info)+
+	    		"kb,TPss=" + getTotalPss(info) + 
+	    		"kb,TSD=" + getTotalSharedDirty(info)+"kb";
 	    	}
 	    }
-	     return 
-	     /*":avail="+(int)(memoryInfo.availMem/1024/1024)+
-	     "MB,LBV="+(int)memoryInfo.threshold/1024/1024+
-	     "MB,"+memoryInfo.lowMemory + */extra;
-
+	     return extra;
 	}
 
 	public android.os.Debug.MemoryInfo[] getMemInfoData(Context context, int[] pids) {
 		ActivityManager manager = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
-		android.os.Debug.MemoryInfo[] memoryInfoArray = manager.getProcessMemoryInfo(pids);
+		android.os.Debug.MemoryInfo[] memoryInfoArray = getProcessMemoryInfo(manager, pids);
 		return memoryInfoArray;
 	}
 
-	public static void systemOut(android.os.Debug.MemoryInfo[] memoryInfoArray) {
+	
+	public void systemOut(android.os.Debug.MemoryInfo[] memoryInfoArray) {
 		for(android.os.Debug.MemoryInfo pidMemoryInfo: memoryInfoArray) {
 			Log.v("kyorohiro", "===================================================");
-			Log.v("kyorohiro", "getTotalPrivateDirty: " + pidMemoryInfo.getTotalPrivateDirty());
-			Log.v("kyorohiro", "getTotalSharedDirty: " + pidMemoryInfo.getTotalSharedDirty());
-			Log.v("kyorohiro", "getTotalPss: " + pidMemoryInfo.getTotalPss());
+			Log.v("kyorohiro", "getTotalPrivateDirty: " + getTotalPrivateDirty(pidMemoryInfo));
+			Log.v("kyorohiro", "getTotalSharedDirty: " + getTotalSharedDirty(pidMemoryInfo));
+			Log.v("kyorohiro", "getTotalPss: " + getTotalPss(pidMemoryInfo));
 			Log.v("kyorohiro", "---------------------------------------------------");
 			Log.v("kyorohiro", "dalvikPrivateDirty: " + pidMemoryInfo.dalvikPrivateDirty);
 			Log.v("kyorohiro", "dalvikPss: " + pidMemoryInfo.dalvikPss);
@@ -80,7 +81,80 @@ public class KyoroMemoryInfo {
 		}
 	}
 
-	static public class MemInfoData {
+	private static final Class<?>[] mGetProcessMemoryInfoSignature = new Class[] {int[].class};
+	private Method mGetProcessMemoryInfo;
+	private Method mGetTotalPrivateDirty;
+	private Method mGetTotalPss;
+	private Method mGetTotalSharedDirty;
 
+	private boolean support = false;
+	private Object[] mGetProcessMemoryInfoArgs = new Object[1];
+
+	public void setMethod() {
+		try {
+			mGetProcessMemoryInfo = ActivityManager.class.getMethod("getProcessMemoryInfo", null);
+			mGetTotalPrivateDirty = ActivityManager.class.getMethod("getTotalPrivateDirty", null);
+			mGetTotalPss = ActivityManager.class.getMethod("getTotalPss", null);
+			mGetTotalSharedDirty = ActivityManager.class.getMethod("getTotalSharedDirty", null);
+			support = true;
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+	}
+	public synchronized android.os.Debug.MemoryInfo[] getProcessMemoryInfo(ActivityManager manager, int[] pids) {
+		if(support){
+			mGetProcessMemoryInfoArgs[0] = pids;
+			Object ret = invokeMethod(manager, mGetProcessMemoryInfo, mGetProcessMemoryInfoArgs);
+			if(ret != null) {
+				return (android.os.Debug.MemoryInfo[])ret;
+			}
+		}
+		return null;
+	}
+
+	public synchronized int getTotalPrivateDirty(android.os.Debug.MemoryInfo o) {
+		if(support){
+			Object ret = invokeMethod(o, mGetTotalPrivateDirty, null);
+			if(ret != null) {
+				return (Integer)ret;
+			}
+		}
+		return 0;
+	}
+
+	public synchronized int getTotalPss(android.os.Debug.MemoryInfo o) {
+		if(support){
+			Object ret = invokeMethod(o, mGetTotalPss, null);
+			if(ret != null) {
+				return (Integer)ret;
+			}
+		}
+		return 0;
+	}
+
+	public synchronized int getTotalSharedDirty(android.os.Debug.MemoryInfo o) {
+		if(support){
+			Object ret = invokeMethod(o, mGetTotalSharedDirty, null);
+			if(ret != null) {
+				return (Integer)ret;
+			}
+		}
+		return 0;
+	}
+
+
+	private Object invokeMethod(Object o, Method method, Object[] args) {
+		try {
+			return  method.invoke(o, args);
+		} catch (InvocationTargetException e) {
+			// Should not happen.
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// Should not happen.
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
