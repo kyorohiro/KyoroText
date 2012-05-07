bb11eb061186a992a961275ee627ccbb8864635b
kyorohiro <kyorohiro@gmail.com>
2012-05-08 01:15:23 +0900

    create and test pdf boolean object

--- KyoroPdfParser/src/info/kyorohiro/helloworld/pdfparser/PdfLexer.java
+++ KyoroPdfParser/src/info/kyorohiro/helloworld/pdfparser/PdfLexer.java
@@ -1,5 +1,7 @@
  package info.kyorohiro.helloworld.pdfparser;
 
+// memo 
+// pdf is written at ascii character with 0-128 and byte stream.
 public class PdfLexer extends Lexer {
 	public static Token SET_SPACE = new Token(1, "<space>", "^\\s|^\t|^\n|^\r");
 	public static Token SET_COMMENT = new Token(2, "<comment>", "^%.*[\n\0]");
@@ -9,23 +11,29 @@ public class PdfLexer extends Lexer {
 	public static Token SET_ARRAY_BEGIN = new Token(6, "<array_begin>", "^\\[");
 	public static Token SET_ARRAY_END = new Token(7, "<array_begin>", "^\\]");
 	public static Token SET_ASCII_BEGIN = new Token(8, "<ascii_begin>", "^\\(");
-	public static Token SET_ASCII_END = new Token(9, "<ascii_end>", "^\\)");
-	public static Token SET_OBJECT_BEGIN = new Token(10, "<object_begin>", "^obj");
-	public static Token SET_OBJECT_END = new Token(11, "<object_end>", "^endobj");
-	public static Token SET_NAME = new Token(12, "<name>", "^/[A-Za-z0-9]+");
-	public static Token SET_IDENTIFY = new Token(13, "<identify>", "^[A-Za-z0-9]+");
+	public static Token SET_ASCII_CHARSET = new Token(9, "<ascii_charset>", "^[\\x00-\\x7F]+");
+	public static Token SET_ASCII_END = new Token(10, "<ascii_end>", "^\\)");
+	public static Token SET_NONASCII_BEGIN = new Token(11, "<nonascii_begin>", "^<");
+	public static Token SET_NONASCII_END = new Token(12, "<nonascii_end>", "^>");
+	public static Token SET_OBJECT_BEGIN = new Token(13, "<object_begin>", "^obj");
+	public static Token SET_OBJECT_END = new Token(14, "<object_end>", "^endobj");
+	public static Token SET_NAME = new Token(15, "<name>", "^/[A-Za-z0-9]+");
+	public static Token SET_IDENTIFY = new Token(16, "<identify>", "^[A-Za-z0-9]+");
+	public static Token SET_DELIMITER = new Token(17, "<identify>", "^[\\(\\)<>\\[\\]{}\\/%]");
+	public static Token SET_BOOLEAN = new Token(18, "<boolean>", "^true|^false");
 
 	public static Token[] SET_UNRETURN_TOKEN_LIST = {
 		SET_SPACE, SET_COMMENT,
 	};
 
 	public static Token[] SET_RETURN_TOKEN_LIST = {
-		SET_EOF, SET_NUMBER,
+		SET_EOF, SET_BOOLEAN, SET_NUMBER,
 		SET_INDEX_BEGIN, SET_INDEX_END,
 		SET_ARRAY_BEGIN, SET_ARRAY_END,
 		SET_ASCII_BEGIN, SET_ASCII_END,
+		SET_NONASCII_BEGIN,SET_NONASCII_END,
 		SET_OBJECT_BEGIN, SET_OBJECT_END, 
-		SET_NAME,SET_IDENTIFY };
+		SET_NAME,SET_IDENTIFY,SET_ASCII_CHARSET };
 
 	public PdfLexer(Text text) {
 		super(text);
---
+++ KyoroPdfParser/src/info/kyorohiro/helloworld/pdfparser/test/pdfobject/TestForPdfBoolean.java
@@ -0,0 +1,48 @@
+package info.kyorohiro.helloworld.pdfparser.test.pdfobject;
+
+import info.kyorohiro.helloworld.pdfparser.GotoException;
+import info.kyorohiro.helloworld.pdfparser.PdfLexer;
+import info.kyorohiro.helloworld.pdfparser.LookaheadParser;
+import info.kyorohiro.helloworld.pdfparser.PdfParser;
+import info.kyorohiro.helloworld.pdfparser.Text;
+import info.kyorohiro.helloworld.pdfparser.pdfobject.PdfAscii;
+import info.kyorohiro.helloworld.pdfparser.pdfobject.PdfBoolean;
+import info.kyorohiro.helloworld.pdfparser.pdfobject.PdfValue;
+import junit.framework.TestCase;
+
+public class TestForPdfBoolean extends TestCase {
+	public static String sTestData001 = "true \n"+"false ";
+	public static String sTestData002 = "tr";
+
+	public void testHello() {
+		android.util.Log.v("pdfparser","hello");
+	}
+
+	public void testBasic00() {
+		PdfLexer lexer = new PdfLexer(new Text(TestForPdfBoolean.sTestData001));
+		PdfParser parser = new PdfParser(lexer);
+		try { 
+			PdfBoolean pars1 = (PdfBoolean)PdfBoolean.builder.createToken(parser);
+			android.util.Log.v("pdfparser","---1--");
+			PdfBoolean pars2 = (PdfBoolean)PdfBoolean.builder.createToken(parser);
+			assertEquals(1, pars1.numOfChild());
+			assertEquals(1, pars2.numOfChild());
+			assertEquals(true, pars1.isTrue());
+			assertEquals(false, pars2.isTrue());
+		} catch (GotoException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+
+	public void testBasic01() {
+		PdfLexer lexer = new PdfLexer(new Text(TestForPdfBoolean.sTestData002));
+		PdfParser parser = new PdfParser(lexer);
+		try { 
+			PdfBoolean pars1 = (PdfBoolean)PdfBoolean.builder.createToken(parser);
+			assertTrue(false);
+		} catch (GotoException e) {
+			assertTrue(true);
+		}
+	}
+}
