1e43a68c556073b55b46cc721da2d9347dfea140
kyorohiro <kyorohiro@gmail.com>
2012-10-06 19:23:14 +0900

    now creating search text

--- KyoroCommon/src/info/kyorohiro/helloworld/display/widget/lineview/edit/Differ.java
+++ KyoroCommon/src/info/kyorohiro/helloworld/display/widget/lineview/edit/Differ.java
@@ -16,8 +16,14 @@ public class Differ {
 		return mLength;
 	}
 
-	public void addLine(int i, CharSequence line) {
-		android.util.Log.v("ll","aa i="+i+",+"+line);
+	private final GetAction mGetAction = new GetAction();
+	public synchronized LineViewData get(LineViewBufferSpec spec, int _index) {
+		mGetAction.setIndex(_index);
+		return mGetAction.get(this, spec);
+	}
+
+	public synchronized void addLine(int i, CharSequence line) {
+		android.util.Log.v("ll", "aa i=" + i + ",+" + line);
 		int len = mLine.size();
 		int index = 0;
 		int start = 0;
@@ -27,20 +33,23 @@ public class Differ {
 			Line l = mLine.get(x);
 			start = index + l.begin();
 			end = start + l.length();
-			android.util.Log.v("ll","aa i=[0]"+start+","+end+","+index);
+			android.util.Log.v("ll", "aa i=[0]" + start + "," + end + ","
+					+ index);
 			// 範囲内か隣接している場合
-//			if (start <= index && index <= end) {
+			// if (start <= index && index <= end) {
 			if (start <= i && i <= end) {
-				android.util.Log.v("ll","aa i=[1]"+(index-start)+",+"+line);
+				android.util.Log.v("ll", "aa i=[1]" + (index - start) + ",+"
+						+ line);
 				find = true;
-				l.insert(i-start, line);
-//				l.insert(index-start, line);
+				l.insert(i - start, line);
+				// l.insert(index-start, line);
 				break;
 			}
 			// 前に存在する場合
-//			else if (index < start) {
+			// else if (index < start) {
 			else if (i < start) {
-				android.util.Log.v("ll","aa i=[2]"+(i-index)+",+"+line);
+				android.util.Log
+						.v("ll", "aa i=[2]" + (i - index) + ",+" + line);
 				mLine.add(x, new AddLine(i - index, line));
 				// mLine.add(index, new AddLine(i-index,line));
 				find = true;
@@ -66,30 +75,35 @@ public class Differ {
 		}
 	}
 
-	public LineViewData get(LineViewBufferSpec spec, int _index) {
-		int i = _index;
-		{
-			int len = mLine.size();
-			int index = 0;
-			int start = 0;
-			int end = 0;
-			boolean find = false;
+	public void checkAllSortedLine(CheckAction action) {
+		int len = mLine.size();
+		int index = 0;
+		int start = 0;
+		int end = 0;
+		boolean find = false;
+		try {
+			action.init();
 			for (int x = 0; x < len; x++) {
 				Line l = mLine.get(x);
 				start = index + l.begin();
 				end = start + l.length();
 				// -ココイガイは使いまわしたい-
-				if (start <= _index && _index < end) {
-					return new LineViewData(l.get(_index - start), Color.BLACK, LineViewData.INCLUDE_END_OF_LINE);
-				} else if (_index < start) {
-					break;
+				if (!action.check(l, start, end)) {
+					return;
 				}
 				// --
 				index = end;
-				i = _index - end;
 			}
+		} finally {
+			action.end();
 		}
-		return spec.get(i);
+	}
+
+	public static abstract class CheckAction {
+		public void init(){};
+		// if return false,  when check action is end. 
+		public abstract boolean check(Line l, int start, int end);
+		public void end(){};
 	}
 
 	public static class AddLine implements Line {
@@ -134,16 +148,57 @@ public class Differ {
 
 	interface Line {
 		public int consume();
-
 		public int begin();
-
 		public int length();
-
 		public void set(int index, CharSequence line);
-
 		public CharSequence get(int i);
-
 		public void insert(int index, CharSequence line);
+	}
+
+	public static class GetAction extends CheckAction {
+		private int mIndex = 0;
+		private int mResult = 0;
+		private CharSequence mRe = "";
+		private boolean mIsDiffer = false;
+		public void setIndex(int index) {
+			mIndex = index;
+		}
+		public boolean isDiffer() {
+			return mIsDiffer;
+		}
+
+		public LineViewData get(Differ differ, LineViewBufferSpec spec) {
+			differ.checkAllSortedLine(this);
+			if(isDiffer()) {
+				return new LineViewData(mRe, Color.BLACK, LineViewData.INCLUDE_END_OF_LINE);
+			} else {
+				android.util.Log.v("text","ll="+mResult+","+mIndex);
+				return spec.get(mResult);
+			}
+		}
+
+		@Override
+		public void init() {
+			mResult = mIndex;
+			mIsDiffer = false;
+		}
+		@Override
+		public boolean check(Line l, int start, int end) {
+			boolean repeat = true;
+			if (start <= mIndex && mIndex < end) {
+				repeat = false;
+				mRe = l.get(mIndex - start);
+				mIsDiffer = true;
+				mResult -= (end-start);
+			} else if (mIndex < start) {
+				repeat = false;
+				mIsDiffer = false;
+			} else {
+				mResult -= (end-start);
+			}
+			android.util.Log.v("text","la="+mResult);
+			return repeat;
+		}
 	}
 
 }
