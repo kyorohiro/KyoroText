03746c30a26cedbf0af2105b40438608ec5392d3
kyorohiro <kyorohiro@gmail.com>
2012-10-27 01:21:03 +0900

    now creating save text

--- KyoroCommon/src/info/kyorohiro/helloworld/display/simple/EditableSurfaceView.java
+++ KyoroCommon/src/info/kyorohiro/helloworld/display/simple/EditableSurfaceView.java
@@ -272,6 +272,6 @@ public class EditableSurfaceView extends MultiTouchSurfaceView {
 		}
 	}
 	public static void log(String log) {
-		android.util.Log.v("kiyo", ""+log);
+	//	android.util.Log.v("kiyo", ""+log);
 	}
 }
