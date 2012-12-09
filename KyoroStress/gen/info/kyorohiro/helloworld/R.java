8f9ff51037aaca89431cac4ffe40db183bd07092
kyorohiro <kyorohiro@gmail.com>
2012-12-10 03:03:51 +0900

    remove gomi

--- KyoroStress/gen/info/kyorohiro/helloworld/R.java
+++ KyoroStress/gen/info/kyorohiro/helloworld/R.java
@@ -8,6 +8,6 @@ package info.kyorohiro.helloworld;
 
 public final class R {
 	public static final class drawable {
-		public static final int ic_launcher = 0x7f020002;
+		public static final int ic_launcher = 0x7f020000;
 	}
 }
