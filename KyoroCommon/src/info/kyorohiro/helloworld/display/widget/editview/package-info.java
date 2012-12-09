9379da7465d926b94880e10403a0462dd22e1af1
kyorohiro <kyorohiro@gmail.com>
2012-12-10 02:56:24 +0900

    add package info

--- KyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/package-info.java
+++ KyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/package-info.java
@@ -2,4 +2,4 @@
  * .display.simple と.widget.lineview のテキスト編集機能拡張したもの。
  * 
  */
-package info.kyorohiro.helloworld.display.simple;
+package info.kyorohiro.helloworld.display.widget.editview;
--- KyoroCommon/src/info/kyorohiro/helloworld/display/widget/lineview/package-info.java
+++ KyoroCommon/src/info/kyorohiro/helloworld/display/widget/lineview/package-info.java
@@ -4,4 +4,4 @@
  *  - 範囲選択ができる。
  *  - フリック操作でスクロールできる。
  */
-package info.kyorohiro.helloworld.display.simple;
+package info.kyorohiro.helloworld.display.widget.lineview;
