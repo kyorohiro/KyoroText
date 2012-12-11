f3e3adc6ef1dcee09a8db7f0bcdaccbc74eae030
kyorohiro <kyorohiro@gmail.com>
2012-12-12 00:05:52 +0900

    now creating KyoroCommon for j2se

--- KyoroCommon/src/info/kyorohiro/helloworld/pfdep/j2se/adapter/EditableComponent.java
+++ KyoroCommon/src/info/kyorohiro/helloworld/pfdep/j2se/adapter/EditableComponent.java
@@ -1,55 +1,12 @@
 package info.kyorohiro.helloworld.pfdep.j2se.adapter;
 
 /*
- * Copyright 2002 Sun Microsystems, Inc. All  Rights Reserved.
- *  
- * Redistribution and use in source and binary forms, with or 
- * without modification, are permitted provided that the following 
- * conditions are met:
- * 
- * -Redistributions of source code must retain the above copyright  
- *  notice, this list of conditions and the following disclaimer.
- * 
- * -Redistribution in binary form must reproduce the above copyright 
- *  notice, this list of conditions and the following disclaimer in 
- *  the documentation and/or other materials provided with the 
- *  distribution.
- *  
- * Neither the name of Sun Microsystems, Inc. or the names of 
- * contributors may be used to endorse or promote products derived 
- * from this software without specific prior written permission.
- * 
- * This software is provided "AS IS," without a warranty of any 
- * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND 
- * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY 
- * EXCLUDED. SUN AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY 
- * DAMAGES OR LIABILITIES  SUFFERED BY LICENSEE AS A RESULT OF OR 
- * RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THE SOFTWARE OR 
- * ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE 
- * FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, 
- * SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER 
- * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF 
- * THE USE OF OR INABILITY TO USE SOFTWARE, EVEN IF SUN HAS BEEN 
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- *  
- * You acknowledge that Software is not designed, licensed or 
- * intended for use in the design, construction, operation or 
- * maintenance of any nuclear facility. 
- */
+*/
 
 import info.kyorohiro.helloworld.display.simple.CommitText;
 import info.kyorohiro.helloworld.display.simple.MyInputConnection;
 
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
