4be0bc62cd1f92f67fc8a67429707e5ae47a914c
kyorohiro <kyorohiro@gmail.com>
2012-12-12 04:04:25 +0900

    now creating KyoroCommon for j2se

--- KyoroCommon/src/info/kyorohiro/helloworld/pfdep/j2se/adapter/EditableComponent.java
+++ KyoroCommon/src/info/kyorohiro/helloworld/pfdep/j2se/adapter/EditableComponent.java
@@ -43,43 +43,52 @@ public class EditableComponent extends JLabel implements KeyListener, FocusListe
 	@Override
     public void keyTyped(KeyEvent event) {
         char keyChar = event.getKeyChar();
+        {
+            int mod = event.getModifiersEx();
 
+            if ((mod & InputEvent.SHIFT_DOWN_MASK) != 0){
+            	System.out.println("+SHIFT");
+            }
+
+            if ((mod & InputEvent.ALT_DOWN_MASK) != 0){
+            	System.out.println(" +ALT");
+            }
+
+            if ((mod & InputEvent.CTRL_DOWN_MASK) != 0){
+            	System.out.println(" +CTRL");
+            }
+        }
     	System.out.println("keyTyped="+keyChar+",keyChar="+event.getKeyChar()+":"+(int)event.getKeyChar()+","+event.getKeyCode());
     	System.out.println("keyTyped ext="+event.getExtendedKeyCode()+","+event.getModifiers());
-    	if(event.getKeyChar() == KeyEvent.VK_CONTROL){
-    		CommitText c = new CommitText(SimpleKeyEvent.KEYCODE_DEL);
-    		//c.isKeyCode();
+		if((InputEvent.CTRL_DOWN_MASK&event.getModifiersEx())==InputEvent.CTRL_DOWN_MASK) {
+			System.out.println("---ctl-[a]-"+(""+((char)('a'+event.getKeyChar()-1))));
+    		CommitText c = new CommitText(""+((char)('a'+event.getKeyChar()-1)),0);
+			c.pushingCtrl(true);  
         	mInputConnection.addCommitText(c);
-    	} else 
+		} else  
     	if(event.getKeyChar() == KeyEvent.VK_BACK_SPACE){
+			System.out.println("-------[b]-"+event.getKeyChar());
     		CommitText c = new CommitText(SimpleKeyEvent.KEYCODE_DEL);
     		//c.isKeyCode();
         	mInputConnection.addCommitText(c);
     	} else if(event.getKeyChar() == KeyEvent.VK_ENTER){
+			System.out.println("-------[c]-"+event.getKeyChar());
     		CommitText c = new CommitText(SimpleKeyEvent.KEYCODE_ENTER);
     		//c.isKeyCode();
         	mInputConnection.addCommitText(c);
     	} else {
-
     		CommitText c = null;
-    		if((InputEvent.CTRL_DOWN_MASK&event.getModifiersEx())==InputEvent.CTRL_DOWN_MASK) {
-    			System.out.println("---ctl--"+(""+(char)('a'+event.getKeyChar()-1)));
-        		c = new CommitText(""+(char)('a'+event.getKeyChar()-1),0);
-    			c.pushingCtrl(true);  
-            	mInputConnection.addCommitText(c);
-    			
-    		} else {
-        		c = new CommitText(""+event.getKeyChar(),1);
-    			c.pushingCtrl(false);  
-            	mInputConnection.addCommitText(c);
-    		}
+    		System.out.println("---ctl-[b]-"+event.getKeyChar());
+    		c = new CommitText(""+event.getKeyChar(),1);
+    		c.pushingCtrl(false);  
+    		mInputConnection.addCommitText(c);
     	}
        	event.consume();
 	}
 
     @Override
     public void keyPressed(KeyEvent event) {
-    	
+    	keyTyped(event);
     }
 
     @Override
