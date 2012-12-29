97bb99f3fe39a9fb8e3977a20360dc8f89fffd40
kyorohiro <kyorohiro@gmail.com>
2012-12-29 16:45:24 +0900

    refactaring now emacs tekina shortcut

--- TestForKyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/shortcut/TestForShortcutStateMachine.java
+++ TestForKyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/shortcut/TestForShortcutStateMachine.java
@@ -14,41 +14,92 @@ public class TestForShortcutStateMachine extends TestCase {
 	public void testFirst() {
 		boolean ret = false;
 		ShortcutStateMachine ssm = new ShortcutStateMachine(TEST_COMMAND);
-		TestTask.clear();
-		ret = ssm.update('g', true, false, null, null);
-		assertEquals(true, ret);
-		assertEquals(1, TestTask.sNumOfAct);
-		assertEquals("g", TestTask.sLastAct);
 
-		TestTask.clear();
-		ret = ssm.update('g', false, false, null, null);
-		assertEquals(false, ret);
-		assertEquals(0, TestTask.sNumOfAct);
-		assertEquals(TEST_COMMAND.length, ssm.getWorkList());
+		{
+			TestTask.clear();
+			ret = ssm.update('g', true, false, null, null);
+			assertEquals(true, ret);
+			assertEquals(1, TestTask.sNumOfAct);
+			assertEquals("g", TestTask.sLastAct);
+		}
+		{
+			TestTask.clear();
+			ret = ssm.update('g', false, false, null, null);
+			assertEquals(false, ret);
+			assertEquals(0, TestTask.sNumOfAct);
+			assertEquals(TEST_COMMAND.length, ssm.getWorkList());
+		}
 
-		TestTask.clear();
-		ret = ssm.update('g', false, true, null, null);
-		assertEquals(false, ret);
-		assertEquals(0, TestTask.sNumOfAct);
-		assertEquals(TEST_COMMAND.length, ssm.getWorkList());
+		{
+			TestTask.clear();
+			ret = ssm.update('g', false, true, null, null);
+			assertEquals(false, ret);
+			assertEquals(0, TestTask.sNumOfAct);
+			assertEquals(TEST_COMMAND.length, ssm.getWorkList());
+		}
+		{
+			TestTask.clear();
+			ret = ssm.update('a', true, false, null, null);
+			assertEquals(true, ret);
+			assertEquals(0, TestTask.sNumOfAct);
+			assertEquals(4, ssm.getWorkList());
+			assertEquals("aa", ssm.getWork(0).getTask().getCommandName());
+			assertEquals("abc", ssm.getWork(1).getTask().getCommandName());
+			assertEquals("abd", ssm.getWork(2).getTask().getCommandName());
+			assertEquals("ad", ssm.getWork(3).getTask().getCommandName());
 
-		TestTask.clear();
-		ret = ssm.update('a', true, false, null, null);
-		assertEquals(true, ret);
-		assertEquals(0, TestTask.sNumOfAct);
-		assertEquals(4, ssm.getWorkList());
-		assertEquals("aa", ssm.getWork(0).getTask().getCommandName());
-		assertEquals("abc", ssm.getWork(1).getTask().getCommandName());
-		assertEquals("abd", ssm.getWork(2).getTask().getCommandName());
-		assertEquals("ad", ssm.getWork(3).getTask().getCommandName());
+			ret = ssm.update('a', false, false, null, null);
+			assertEquals(true, ret);
+			assertEquals(1, TestTask.sNumOfAct);
+			assertEquals(8, ssm.getWorkList());
+			assertEquals(1, TestTask.sNumOfAct);
+			assertEquals("aa", TestTask.sLastAct);
+		}
+		{	
+			TestTask.clear();
+			ret = ssm.update('a', true, false, null, null);
+			assertEquals(true, ret);
+			assertEquals(0, TestTask.sNumOfAct);
+			assertEquals(4, ssm.getWorkList());
+			assertEquals("aa", ssm.getWork(0).getTask().getCommandName());
+			assertEquals("abc", ssm.getWork(1).getTask().getCommandName());
+			assertEquals("abd", ssm.getWork(2).getTask().getCommandName());
+			assertEquals("ad", ssm.getWork(3).getTask().getCommandName());
+
+			ret = ssm.update('b', false, false, null, null);
+			assertEquals(true, ret);
+			assertEquals(0, TestTask.sNumOfAct);
+			assertEquals(2, ssm.getWorkList());
+			assertEquals("abc", ssm.getWork(0).getTask().getCommandName());
+			assertEquals("abd", ssm.getWork(1).getTask().getCommandName());
+
+			ret = ssm.update('d', true, false, null, null);
+			assertEquals(true, ret);
+			assertEquals(1, TestTask.sNumOfAct);
+			assertEquals(8, ssm.getWorkList());
+			assertEquals(1, TestTask.sNumOfAct);
+			assertEquals("abd", TestTask.sLastAct);
+		}		
+		{
+			TestTask.clear();
+			ret = ssm.update('d', false, true, null, null);
+			assertEquals(true, ret);
+			assertEquals(1, TestTask.sNumOfAct);
+			assertEquals("d1", TestTask.sLastAct);
+			ret = ssm.update('d', true, true, null, null);
+			assertEquals(true, ret);
+			assertEquals(2, TestTask.sNumOfAct);
+			assertEquals("d2", TestTask.sLastAct);
+		}
+		
 	}
 
 	public static Command[] TEST_COMMAND = {
 		new Command(new CommandPart[]{new CommandPart('g', true, false)}, new TestTask("g")),
-		new Command(new CommandPart[]{new CommandPart('a', true, false), new CommandPart('a', true, false)}, new TestTask("aa")),
-		new Command(new CommandPart[]{new CommandPart('a', true, false), new CommandPart('b', true, false), new CommandPart('c', true, false)}, new TestTask("abc")),
-		new Command(new CommandPart[]{new CommandPart('a', true, false), new CommandPart('b', true, false), new CommandPart('d', true, false)}, new TestTask("abd")),
-		new Command(new CommandPart[]{new CommandPart('a', true, false), new CommandPart('d', true, false)}, new TestTask("ad")),
+		new Command(new CommandPart[]{new CommandPart('a', true, false), new CommandPart('a', false, false)}, new TestTask("aa")),
+		new Command(new CommandPart[]{new CommandPart('a', true, false), new CommandPart('b', false, false), new CommandPart('c', true, false)}, new TestTask("abc")),
+		new Command(new CommandPart[]{new CommandPart('a', true, false), new CommandPart('b', false, false), new CommandPart('d', true, false)}, new TestTask("abd")),
+		new Command(new CommandPart[]{new CommandPart('a', true, false), new CommandPart('d', false, false)}, new TestTask("ad")),
 		new Command(new CommandPart[]{new CommandPart('c', true, false)}, new TestTask("c")),
 		new Command(new CommandPart[]{new CommandPart('d', false, true)}, new TestTask("d1")),
 		new Command(new CommandPart[]{new CommandPart('d', true, true)}, new TestTask("d2")),
