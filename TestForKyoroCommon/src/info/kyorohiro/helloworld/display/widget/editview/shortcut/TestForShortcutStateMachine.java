82027dfbf9d7a6b5a35c36a5b325a9c8d9217d19
kyorohiro <kyorohiro@gmail.com>
2012-12-29 15:39:01 +0900

    refactaring now emacs tekina shortcut

---
+++ TestForKyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/shortcut/TestForShortcutStateMachine.java
@@ -0,0 +1,9 @@
+package info.kyorohiro.helloworld.display.widget.editview.shortcut;
+
+import junit.framework.TestCase;
+
+public class TestForShortcutStateMachine extends TestCase {
+	public void testHello() {
+		assertTrue(false);
+	}
+}
