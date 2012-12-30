a0f58c9a6778b9a901ce1c5be22d0a6dd524354f
kyorohiro <kyorohiro@gmail.com>
2012-12-30 23:05:05 +0900

    now creating isearch-forward

--- KyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/task/SearchTask.java
+++ KyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/task/SearchTask.java
@@ -5,6 +5,7 @@ import info.kyorohiro.helloworld.display.widget.editview.EditableLineViewBuffer;
 import info.kyorohiro.helloworld.display.widget.lineview.MyCursor;
 import info.kyorohiro.helloworld.text.KyoroString;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,21 +19,84 @@ public class SearchTask implements Runnable {
 
 	@Override
 	public void run() {
-		Pattern pattern = Pattern.compile(mRegex);
 		MyCursor cursor = mTargetView.getLeft();
 		int index = cursor.getCursorCol();
 		int row = cursor.getCursorRow();
 		EditableLineViewBuffer buffer = (EditableLineViewBuffer)mTargetView.getLineViewBuffer();
+		Line l = new Line(mRegex);
+		boolean f = true;
 		while(index<buffer.getNumberOfStockedElement()) {
 			KyoroString str = buffer.get(index);
-			Matcher m = pattern.matcher(str.toString());
-			if(m.find()) {
-				mTargetView.getLeft().setCursorCol(index);
-				mTargetView.getLeft().setCursorRow(m.end());
-				mTargetView.recenter();
-				break;
+			if(f) {
+				l.add(str,row, str.length());
+				f = false;
+			} else {
+				l.add(str, 0, str.length());
+			}
+			if(str.includeLF()) {
+				if(l.find()) {
+					mTargetView.getLeft().setCursorCol(l.getY()+index);
+					mTargetView.getLeft().setCursorRow(l.getX()+(l.getY()==0?row:0));
+					mTargetView.recenter();
+					break;
+				}
 			}
 			index++;
+		}
+	}
+
+	//
+	// KyoroText's Managed Text data dont't divide per CRLF
+	// This class create group per CRLF. and find
+	public static class Line {
+		private StringBuffer mBuffer = new StringBuffer();
+		private ArrayList<Integer> mLength = new ArrayList<Integer>(10);
+		private Pattern mPattern = null;
+		private int mX = 0;
+		private int mY = 0;
+		
+		
+		public Line(String regex) {
+			mPattern = Pattern.compile(regex);
+		}
+
+		public void add(CharSequence str, int begin, int end) {
+			mBuffer.append(str.subSequence(begin, end));
+		}
+
+		public void clear() {
+			mLength.clear();
+			mBuffer = new StringBuffer();
+			mX = 0;
+			mY = 0;
+		}
+
+		public boolean find() {
+			Matcher mMatcher = mPattern.matcher(mBuffer.toString());
+			boolean ret = mMatcher.find();
+			if(ret) {
+				int start = mMatcher.start();
+				int end = mMatcher.end();
+				int len = 0;
+				int prev = 0;
+				for(int i=0;i<mLength.size();i++) {
+					len +=mLength.get(i);
+					if(end>=len) {
+						mY = i;
+						mX = end-prev;
+					}
+					prev = len;
+				}
+			}
+			return ret;
+		}
+
+		public int getX() {
+			return mX;
+		}
+
+		public int getY() {
+			return mY;
 		}
 	}
 }
