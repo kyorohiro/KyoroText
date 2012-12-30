3a3fcf0c73b700de19a45cecf301158092290465
kyorohiro <kyorohiro@gmail.com>
2012-12-31 02:12:07 +0900

    now creating isearch-forward

---
+++ TestForKyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/task/TestForSearchTask.java
@@ -0,0 +1,10 @@
+package info.kyorohiro.helloworld.display.widget.editview.task;
+
+import junit.framework.TestCase;
+
+public class TestForSearchTask extends TestCase {
+
+	public void testHello() {
+		
+	}
+}
