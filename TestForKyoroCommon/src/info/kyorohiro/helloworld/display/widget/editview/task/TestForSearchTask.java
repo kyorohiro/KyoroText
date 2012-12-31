51dc0e211ddf3d1aebcc248a7d78fd51709ad88b
kyorohiro <kyorohiro@gmail.com>
2012-12-31 15:15:38 +0900

    now creating isearch-forward

--- TestForKyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/task/TestForSearchTask.java
+++ TestForKyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/task/TestForSearchTask.java
@@ -13,13 +13,56 @@ public class TestForSearchTask extends TestCase {
 		
 	}
 
+	//
+	// following bc search pattern
+	// { "abcbb", "cgh\r\n", "ijkla" }
+	//
+	public void testSecound() {
+		
+	}
+
+
 	public void testFirst() {
 		{
-			String[] data = { "abcde", "fgh\r\n", "ijkla" };
+			String[] data = { "abcbe", "fgh\r\n", "ijkla" };
 			EmptyLineViewBufferSpecImpl spec = new EmptyLineViewBufferSpecImpl(5);
 			setData(data, spec);
 			EditableLineView view = new EditableLineView(spec, 12, 100);
 
+			{
+				{
+					android.util.Log.v("kiyo","--4--");
+					SearchTask task = new SearchTask(view, "b");
+					AsyncronousTask atask = new AsyncronousTask(task);
+					Thread t = new Thread(atask);
+					view.getLeft().setCursorCol(0);
+					view.getLeft().setCursorRow(0);
+					t.start();
+					atask.waitForTask();
+					assertEquals(2, view.getLeft().getCursorRow());
+					assertEquals(0, view.getLeft().getCursorCol());
+				}
+				{
+					android.util.Log.v("kiyo","--5--");
+					SearchTask task = new SearchTask(view, "a");
+					AsyncronousTask atask = new AsyncronousTask(task);
+					Thread t = new Thread(atask);
+					t.start();
+					atask.waitForTask();
+					assertEquals(4, view.getLeft().getCursorRow());
+					assertEquals(0, view.getLeft().getCursorCol());
+				}
+				{
+					android.util.Log.v("kiyo","--6--");
+					SearchTask task = new SearchTask(view, "a");
+					AsyncronousTask atask = new AsyncronousTask(task);
+					Thread t = new Thread(atask);
+					t.start();
+					atask.waitForTask();
+					assertEquals(2, view.getLeft().getCursorRow());
+					assertEquals(0, view.getLeft().getCursorCol());
+				}
+			}
 			{
 				SearchTask task = new SearchTask(view, "f");
 				AsyncronousTask atask = new AsyncronousTask(task);
