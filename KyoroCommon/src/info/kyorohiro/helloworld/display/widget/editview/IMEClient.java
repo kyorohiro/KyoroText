6135ae81e4fb105c717447c1b96029380e7c6352
kyorohiro <kyorohiro@gmail.com>
2013-01-02 01:18:59 +0900

    house keeping now

--- KyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/IMEClient.java
+++ KyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/IMEClient.java
@@ -1,5 +1,7 @@
 package info.kyorohiro.helloworld.display.widget.editview;
 
+//
+//todo don't fix
 public interface IMEClient {
 	public void setCursor(int row, int col);
 	public void pushCommit(CharSequence text, int cursor) ;
