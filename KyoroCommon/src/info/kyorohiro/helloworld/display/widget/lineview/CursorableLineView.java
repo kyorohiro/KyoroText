d97d44c439bec56aec9c6db6fb827ef012ad534b
kyorohiro <kyorohiro@gmail.com>
2013-01-02 01:33:10 +0900

    mod func . moveable cursor at view mode now

--- KyoroCommon/src/info/kyorohiro/helloworld/display/widget/lineview/CursorableLineView.java
+++ KyoroCommon/src/info/kyorohiro/helloworld/display/widget/lineview/CursorableLineView.java
@@ -115,7 +115,7 @@ public class CursorableLineView extends LineView {
 		if (super.onTouchTest(x, y, action)) {
 			return true;
 		} else {
-			if (mMode.toString().startsWith(MODE_EDIT)) {
+			if (mMode.toString().startsWith(MODE_EDIT)||mMode.toString().startsWith(MODE_VIEW)) {
 				if(action == SimpleMotionEvent.ACTION_DOWN) {
 					mIsEditClickActionX = x;
 					mIsEditClickActionY = y; 
