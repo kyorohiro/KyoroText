60e1a0609d1bd6d9d13ced775f7e5c131a9f986c
kyorohiro <kyorohiro@gmail.com>
2013-01-03 23:33:53 +0900

    house keeping now

--- KyoroCommon/src/info/kyorohiro/helloworld/display/widget/editview/differ/package-info.java
+++
@@ -1,7 +0,0 @@
-/*
- * EditableLineView use BigData
- * Android can not stock treated data in heap 
- * so Kyoro Text stock diff data only. 
- * save editing date short.
- */
-package info.kyorohiro.helloworld.display.widget.editview.differ;
