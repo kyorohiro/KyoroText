3ef39e3eefea7b8b6587c8dd02ff3526816f0c00
kyorohiro <kyorohiro@gmail.com>
2013-01-18 20:52:43 +0900

    house keeping

--- TestForKyoroCommon/src/info/kyorohiro/helloworld/util/arraybuilder/TestForByteArrayBuilder.java
+++ TestForKyoroCommon/src/info/kyorohiro/helloworld/util/arraybuilder/TestForByteArrayBuilder.java
@@ -4,11 +4,37 @@ import junit.framework.TestCase;
 
 public class TestForByteArrayBuilder extends TestCase {
 
+
 	private void log(String log) {
 		android.util.Log.v("kiyo","#TestForByteArrayBuilder#"+log);
 	}
 
+
 	public void testHello() {
 		log("testHello");
+	}
+
+	public void testA() {
+		ByteArrayBuilder builder = new ByteArrayBuilder();
+		byte[] testdata = new byte[1024];
+		for(int i=0;i<testdata.length;i++) {
+			testdata[i] = (byte)i;
+		}
+
+		for(int i=0;i<testdata.length;i++) {
+			builder.append(testdata[i]);
+			// check
+			{
+				byte[] result = builder.getBuffer(); 
+				int s = 0;
+				int e = builder.length();
+				assertEquals(i+1, e);
+				for(int j=s;j<e;j++) {
+					assertEquals((byte)j, result[j]);
+				}
+			}
+		}
+
+		
 	}
 }
