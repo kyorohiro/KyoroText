d999446b93ba4bcc0afc80022e73e1d8884a443f
kyorohiro <kyorohiro@gmail.com>
2013-01-18 20:43:12 +0900

    house keeping

---
+++ TestForKyoroCommon/src/info/kyorohiro/helloworld/util/arraybuilder/TestForByteArrayBuilder.java
@@ -0,0 +1,14 @@
+package info.kyorohiro.helloworld.util.arraybuilder;
+
+import junit.framework.TestCase;
+
+public class TestForByteArrayBuilder extends TestCase {
+
+	private void log(String log) {
+		android.util.Log.v("kiyo","#TestForByteArrayBuilder#"+log);
+	}
+
+	public void testHello() {
+		log("testHello");
+	}
+}
