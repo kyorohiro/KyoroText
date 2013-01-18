8862fe76e4f7476df5e38daaf86270eedc0fb0bf
kyorohiro <kyorohiro@gmail.com>
2013-01-18 20:58:28 +0900

    house keeping

--- TestForKyoroCommon/src/info/kyorohiro/helloworld/util/arraybuilder/TestForByteArrayBuilder.java
+++ TestForKyoroCommon/src/info/kyorohiro/helloworld/util/arraybuilder/TestForByteArrayBuilder.java
@@ -14,7 +14,7 @@ public class TestForByteArrayBuilder extends TestCase {
 		log("testHello");
 	}
 
-	public void testA() {
+	public void testAppend() {
 		ByteArrayBuilder builder = new ByteArrayBuilder();
 		byte[] testdata = new byte[1024];
 		for(int i=0;i<testdata.length;i++) {
@@ -22,6 +22,46 @@ public class TestForByteArrayBuilder extends TestCase {
 		}
 
 		for(int i=0;i<testdata.length;i++) {
+			assertEquals(i, builder.length());
+			builder.append(testdata[i]);
+			// check
+			{
+				byte[] result = builder.getBuffer(); 
+				int s = 0;
+				int e = builder.length();
+				assertEquals(i+1, e);
+				for(int j=s;j<e;j++) {
+					assertEquals((byte)j, result[j]);
+				}
+			}
+		}		
+	}
+
+	public void testClear() {
+		ByteArrayBuilder builder = new ByteArrayBuilder();
+		byte[] testdata = new byte[1024];
+		for(int i=0;i<testdata.length;i++) {
+			testdata[i] = (byte)i;
+		}
+
+		builder.clear();
+		for(int i=0;i<testdata.length;i++) {
+			assertEquals(i, builder.length());
+			builder.append(testdata[i]);
+			// check
+			{
+				byte[] result = builder.getBuffer(); 
+				int s = 0;
+				int e = builder.length();
+				assertEquals(i+1, e);
+				for(int j=s;j<e;j++) {
+					assertEquals((byte)j, result[j]);
+				}
+			}
+		}		
+		builder.clear();
+		for(int i=0;i<testdata.length;i++) {
+			assertEquals(i, builder.length());
 			builder.append(testdata[i]);
 			// check
 			{
@@ -34,7 +74,47 @@ public class TestForByteArrayBuilder extends TestCase {
 				}
 			}
 		}
-
-		
 	}
+
+	public void testSetBufferLength() {
+		ByteArrayBuilder builder = new ByteArrayBuilder();
+		byte[] testdata = new byte[1024];
+		for(int i=0;i<testdata.length;i++) {
+			testdata[i] = (byte)i;
+		}
+
+		builder.clear();
+		for(int i=0;i<testdata.length;i++) {
+			assertEquals(i, builder.length());
+			builder.append(testdata[i]);
+			builder.setBufferLength(i+2);
+			// check
+			{
+				byte[] result = builder.getBuffer(); 
+				int s = 0;
+				int e = builder.length();
+				assertEquals(i+1, e);
+				for(int j=s;j<e;j++) {
+					assertEquals((byte)j, result[j]);
+				}
+			}
+		}		
+		builder.clear();
+		for(int i=0;i<testdata.length;i++) {
+			assertEquals(i, builder.length());
+			builder.setBufferLength(i+2);
+			builder.append(testdata[i]);
+			// check
+			{
+				byte[] result = builder.getBuffer(); 
+				int s = 0;
+				int e = builder.length();
+				assertEquals(i+1, e);
+				for(int j=s;j<e;j++) {
+					assertEquals((byte)j, result[j]);
+				}
+			}
+		}
+	}
+
 }
